10e2471239ecfe1e4fb37c1b695cd3f1fa11980b
KrimsonJosh <jumoinzad@gmail.com>
2024-08-15 19:29:15 -0500

    complete basic stripe session api route

--- app/checkout_sessions.js
+++ app/checkout_sessions.js
@@ -20,7 +20,7 @@ export async function POST(req) {
               product_data: {
                 name: 'Pro subscription',
               },
-              unit_amount: 1000, // $10.00 in cents
+              unit_amount: formatAmountForStripe(10, 'usd'), // $10.00// $10.00 in cents
               recurring: {
                 interval: 'month',
                 interval_count: 1,
